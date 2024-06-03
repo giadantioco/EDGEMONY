fa743ca6c5550678d5fb174bb7619e86fe75963c
giadantioco <giada.antioco@gmail.com>
2024-06-03 15:30:06 +0200

    third commit

--- corso/04-JS_inDEEP/lezioni/1-git.js
+++ corso/04-JS_inDEEP/lezioni/1-git.js
@@ -1,5 +1,15 @@
-// GIT è un programma vhe ti permette di fare azioni sul terminale
+// GIT è un programma che ti permette di fare azioni sul terminale
 
 // CODICE LOCALE -> 
-// git add . git add nomeFile.js
-// 
+// git add . -> per aggiungere cartella | git add nomeFile.js -> per aggiungere file specifico in cartella (in locale)
+// git commit -m " " -> come salvare i file aggiunti (in locale)
+// git push -> per spingere il commit (salvataggio dei file aggiunti) su branch remoto
+
+// git status -> per sapere se cartella è inizializzata (git add .)
+// git clone :url -> per poter clonare una repositori remota e salvarla in locale
+// git log -> visualizza tutti i commit nella cronologia di un repository
+// git ls remote -> per vedere remote path
+
+// ---------------------------------------------------------------------------
+
+
