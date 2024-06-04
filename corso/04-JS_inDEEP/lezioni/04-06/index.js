0ab4a982949518e777eb4cce06b6b096d170f2d1
giadantioco <giada.antioco@gmail.com>
2024-06-04 18:28:55 +0200

    04-06-24

--- corso/04-JS_inDEEP/lezioni/04-06/index.js
+++ corso/04-JS_inDEEP/lezioni/04-06/index.js
@@ -1,21 +1,47 @@
-let number = 0;
+const div = document.querySelector('.counter');
+const p = document.createElement('p');
+const stopButton = document.createElement('button');
+const startButton = document.createElement('button');
 
-const loggerFn = (arg) => {
+stopButton.className = 'stop';
+startButton.className = 'start';
+startButton.textContent = 'Start';
+stopButton.textContent = 'Stop';
 
-    console.log('logggerFn', arg)
+p.textContent = new Date();
+
+div.append(startButton, stopButton);
+
+// Funzione che esegue l'interval e restituisce l'ID dell'interval
+// per poterlo fermare successivamente.
+const runInterval = () => {
+
+    // setInterval metodo che accetta una callback e un valore numerico (ms)
+    // il valore numerico indica quanto tempo dura l'intervallo (delay)
+    // la callback verrà eseguita ogni 1000ms in questo caso
+    const id = setInterval(() => {
+        p.textContent = new Date();
+        console.log(p.textContent)
+    }, 1000)
+
+    return id;
 }
 
-loggerFn('primo log')
+// inizializziamo intervalId con la prima esecuzione della funzione runInterval()
+let intervalId = runInterval();
 
-loggerFn('secondo log')
+// al click di startButton puliamo eventuali interval già in esecuzione
+// ne facciamo partire uno nuovo
+startButton.addEventListener('click', () => {
+    clearInterval(intervalId)
+    intervalId = runInterval();
+});
 
-loggerFn('terzo log')
+// al click di stopButton fermiamo l'interval corrente
+// log sull'id attuale dell'interval fermato.
+stopButton.addEventListener('click', () => {
+    console.log(intervalId);
+    clearInterval(intervalId)
+});
 
-loggerFn(number)
-
-setInterval(() => {
-    number = number++
-    loggerFn('dentro interval')
-}, 5000)
-
-loggerFn('quarto log')
+div.appendChild(p);
